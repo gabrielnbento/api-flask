712f47e541a481cf9b1b1d8b65ea7e6165377d2e
Gabriel Bento <gabriel.bento@picpay.com>
2025-01-03 08:43:01 -0300

    commitando a api

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+df = pd.read_csv('tickets.csv')
+@app.route('/')
+def index():
+    return jsonify({'message': 'Hello World'})
+
+@app.route('/all-tickets', methods=['GET'])
+def all_tickets():
+    return jsonify(df.to_dict())
+
+@app.route('/ticket/<string:ticket_id>', methods=['GET'])
+def get_ticket(ticket_id):
+    ticket = df.where(df['Numero_do_Voo'] == ticket_id).dropna()
+    return jsonify(ticket.to_dict())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
