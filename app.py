5e4a960ca3975f88c0655ea9da5c2aea0a22a545
Gabriel Bento <gabriel.bento@picpay.com>
2025-01-03 11:20:52 -0300

    alteraçoes no app.py retirando o jsonify

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
-from flask import Flask, request, jsonify
+from flask import Flask, Response
+import json
 import pandas as pd
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 df = pd.read_csv('tickets.csv')
+
 @app.route('/')
 def index():
-    return jsonify({'message': 'Hello World'})
+    return '{"message": "Hello World"}'
 
 @app.route('/all-tickets', methods=['GET'])
 def all_tickets():
-    return jsonify(df.to_dict())
+    response_data = json.dumps(df.to_dict('records'), ensure_ascii=False)
+    return Response(response_data, mimetype='application/json; charset=utf-8')
 
-@app.route('/ticket/<string:ticket_id>', methods=['GET'])
+@app.route('/ticket/<ticket_id>', methods=['GET'])
 def get_ticket(ticket_id):
     ticket = df.where(df['Numero_do_Voo'] == ticket_id).dropna()
-    return jsonify(ticket.to_dict())
+    ticket_data = ticket.to_dict('records')
 
+    if ticket_data:
+        response_data = json.dumps(ticket_data[0], ensure_ascii=False)
+  
+        return Response(response_data, mimetype='application/json; charset=utf-8')
+    else:
+        response_data = json.dumps({'error': 'Ticket não encontrado'}, ensure_ascii=False)
+        return Response(response_data, mimetype='application/json; charset=utf-8', status=404)
 
 if __name__ == '__main__':
     app.run(debug=True)
